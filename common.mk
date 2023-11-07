39e202caac02e2d76e9c5b5492f27c1bc5d12110
Dylan Neve <dylanneve1@gmail.com>
2023-11-07 15:04:33 +0000

    exynos9810-common: use DRM widevine HAL from N770FXXU8HVK5
    - Also remove vendor.samsung.hardware.security.widevine.keyprovisioning@1.0-service

--- common.mk
+++ common.mk
@@ -97,9 +97,7 @@ endif
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl:32 \
-    android.hardware.drm@1.0-service \
-    android.hardware.drm@1.3.vendor:32 \
+    android.hardware.drm@1.3.vendor \
     android.hardware.drm@1.4-service.clearkey
 
 # fastbootd
