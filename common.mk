astraosreborn/device_samsung_exynos9810-common-V2

exynos9810-common: migrate to AIDL clearkey DRM HAL

common.mk
@@ -105,7 +105,7 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl:32 \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.3.vendor:32 \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm-service.clearkey
 
 # fastbootd
 PRODUCT_PACKAGES += \

lilium-9810/device_samsung_exynos9810-common

common.mk
@@ -98,7 +98,7 @@ endif
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
-    android.hardware.drm@1.4-service.clearkey
+    aandroid.hardware.drm-service.clearkey
 
 # fastbootd
 PRODUCT_PACKAGES += \